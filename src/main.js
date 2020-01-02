b2f67ddde921d825c28b01cfca02363a1e406ecc
Gavin Hardaker <ghardaker587@gmail.com>
2020-01-02 08:02:10 +0200

    analytics

--- src/main.js
+++ src/main.js
@@ -4,3 +4,14 @@ require('~/assets/css/style.sass')
 export default function (Vue) {
   Vue.component('Layout', DefaultLayout)
 }
+
+module.exports = {
+  plugins: [
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-155217022-1'
+      }
+    }
+  ]
+}
