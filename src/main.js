00e8b8f14c64bdcc9cb637885fa7c757b32ccf02
Gavin Hardaker <ghardaker587@gmail.com>
2020-01-06 06:41:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import DefaultLayout from '~/layouts/Default.vue'
 require('~/assets/css/style.sass')
-import carousel from 'vue-owl-carousel'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import carousel from 'vue-owl-carousel'
 
 export default function (Vue) {
   Vue.component('Layout', DefaultLayout)
